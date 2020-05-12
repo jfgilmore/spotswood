5bc5a01b17d733ca59729ed42675000c40f91241
Joshua Gilmore <djsounddog@gmail.com>
2020-05-12 22:18:15 +1000

    Started updating devise views to show all user fields. Added avatar image to model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :lockable
-
-  enum role: %i[Guest CommunityUser CommunityModerator Moderator Admin]
+  :recoverable, :rememberable, :validatable,
+  :lockable
 
   validates :name, presence: true, length: { in: 2...50 }
   validates :email, presence: true
   validates :phone, numericality: { only_integer: true }, length: { in: 8...10 }
-
   # Code of conduct validated but not saved as all users MUST accept
   validates :coc, acceptance: true
+
+  enum role: %i[Guest CommunityUser CommunityModerator Moderator Admin]
+
+  has_one_attached :avatar
 end
