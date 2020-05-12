5eb9878679f7fa84d94f2f77c349793a4ec6540d
Joshua Gilmore <djsounddog@gmail.com>
2020-05-12 18:46:56 +1000

    Cleaned up migrations

--- db/schema.rb
+++ db/schema.rb
@@ -16,16 +16,19 @@ ActiveRecord::Schema.define(version: 2020_05_12_062425) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "name", default: "", null: false
+    t.text "name", null: false
     t.integer "phone"
-    t.integer "role"
+    t.integer "role", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
