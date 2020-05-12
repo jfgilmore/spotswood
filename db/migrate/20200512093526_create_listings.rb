ccb27e575c5ad01297e79237cc561a91809e6843
Joshua Gilmore <djsounddog@gmail.com>
2020-05-12 19:45:19 +1000

    Added listing model and migration. Began defining validations.

---
+++ db/migrate/20200512093526_create_listings.rb
@@ -0,0 +1,16 @@
+class CreateListings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :listings do |t|
+      t.text :name, null: false
+      t.datetime :when
+      t.text :location
+      t.text :why
+      t.integer :cost, default: 0
+      t.text :summary, null: false
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
