10b46dd460e0ac294dfbf122dc4b60f442e84fd1
Joshua Gilmore <djsounddog@gmail.com>
2020-05-13 16:45:37 +1000

    Added Gravatar as default/placeholder image for users.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,12 @@ module ApplicationHelper
     if user.avatar.attached?
       user.avatar.variant(resize: "#{size}x#{size}!")
     else
-      gravatar_image_url(user.email, size: size)
+      gravatar_image_url(user)
     end
   end
+
+  def gravatar_image_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    "https://secure.gravatar.com/avatar/#{gravatar_id}"
+   end
 end
