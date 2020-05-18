c6de5412e4ac8927b99b67d973a081f0d62b1097
Joshua Gilmore <djsounddog@gmail.com>
2020-05-18 11:09:29 +1000

    Added & styled listings index

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
 
   root to: 'welcome#index'
+  resources :listings
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
