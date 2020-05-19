ccff1482856e1ccc445d66e30017a79a9c3d70bf
Joshua Gilmore <djsounddog@gmail.com>
2020-05-19 21:18:23 +1000

    Added selected interaction highlight to listing view.

--- app/helpers/interactions_helper.rb
+++ app/helpers/interactions_helper.rb
@@ -1,2 +1,9 @@
 module InteractionsHelper
+  def button_class_value listing, user_action
+    if current_user.interactions.find_by(listing_id: listing.id).user_action == user_action[0]
+      'btn btn-success btn-sm'
+    else
+      'btn btn-dark btn-sm'
+    end
+  end
 end
--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -6,4 +6,12 @@ module ListingsHelper
   def user_interaction listing
     listing.interactions.find_by(user: current_user)
   end
+
+  def class_value listing
+    if user_interaction? listing
+      'img-thumbnail border border-success'
+    else
+      'img-thumbnail border border-light'
+    end
+  end
 end
