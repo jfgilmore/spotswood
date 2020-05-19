4e7c87cf0e14d61309650d14af188cab226cb0b0
Joshua Gilmore <djsounddog@gmail.com>
2020-05-19 12:39:08 +1000

    Completed edit/new listing pages. Enabled some html tags for content description. Improved listing validations.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,33 +2,30 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_many_attached :images, dependent: :destroy
 
+  validate :image_type
+  validate :cannot_be_in_the_past
   validates :name, length: { in: 2...40 }
   validates :summary, length: { in: 22...120 }
   validates :location, length: { in: 6...200 }
   validates :cost, numericality: { only_integer: true }
-  validate :cannot_be_in_the_past
-  validate :image_type
 
+  # Validation helpers
   def cannot_be_in_the_past
-    errors.add(:when, 'New events can only happen in the future') unless Time.zone.now.to_i > time.to_i
+    errors.add(:time, 'New events can only happen in the future') unless Time.zone.now.to_i < time.to_i
   end
 
+  def image_type
+    images.each do |image|
+      errors.add(:images, 'Only jpeg, jpg, png, and gif formats accepted') unless image.content_type.in?(%('image/jpeg image/jpg image/png image/gif'))
+    end
+  end
+
+  # Image helpers
   def display_size(image)
-    return self.images[image].variant(resize_to_fill: [1000, 700, { gravity: 'Center' }]).processed
+    return self.images[image].variant(resize_to_fill: [1200, 900, { gravity: 'Northwest' }]).processed
   end
 
   def thumbnail(input, size = 50)
     return self.images[input].variant(resize: "#{size}x#{size}!")
   end
-
-  def image_type
-    if images.attached?
-      images.each do |image|
-        if !image.content_type.in?(%('image/jpeg image/jpg image/png image/gif'))
-          errors.add(:images, 'Only jpg, png, and gif formats accepted.')
-        end
-      end
-    end
-  end
 end
-# gravity: "center",
